645098800a047e6211ed4f821c4bd4251823d62f
dreyhiflden <dreyhiflden@gmail.com>
2019-02-19 23:53:30 +0200

    Add working prototype

--- ball.js
+++ ball.js
@@ -1,8 +1,26 @@
-let el = document.querySelector('#field');
+const field = document.querySelector('#field');
+const ball = document.querySelector('#ball');
+const borderWidth = (field.offsetWidth - field.clientWidth) / 2;
 
-el.addEventListener('click', func);
+let posY = field.offsetTop;
+let posX = field.offsetLeft;
 
-function func(event) {
-  alert(event.type + " на " + event.currentTarget);
-  alert(event.clientX + ":" + event.clientY);
+
+field.addEventListener('click', placeBall);
+
+function placeBall(event) {
+
+  console.log(event);
+
+  console.log(posY);
+
+  if ((event.clientY - posY + (event.pageY - event.clientY)) < posY) {
+    console.log(`${(event.clientY - posY + (event.pageY - event.clientY)) < posY}`);
+  }
+
+  ball.style.cssText = `top:${(event.clientY - posY + (event.pageY - event.clientY))}px;
+   margin-top: -${(ball.offsetHeight / 2) + borderWidth}px;
+   margin-left: -${(ball.offsetWidth / 2) + borderWidth}px;
+   left:${event.clientX - posX + (event.pageX - event.clientX)}px`;
+
 }

a568c2d7257da722ca7530adc8ddba842f24e7df
dreyhiflden <dreyhiflden@gmail.com>
2019-02-27 01:01:54 +0200

    Complete ball task

--- ball.js
+++ ball.js
@@ -3,93 +3,32 @@ const ball = document.querySelector('#ball');
 const ballRadius = ball.offsetWidth / 2;
 const borderWidth = (field.offsetWidth - field.clientWidth) / 2;
 const cords = field.getBoundingClientRect();
-
-const body = document.querySelector('body');
-
-let posTop = cords.top + window.scrollY;
-let posLeft = cords.left + window.scrollX;
-
-console.log(`posY = field.offsetTop = ${field.offsetTop}`);
-console.log(`posY = cords.top = ${cords.top}`);
-console.log(`posX = field.offsetLeft = ${field.offsetLeft}`);
-console.log(`posX = cords.left = ${cords.left}`);
-console.log(`ball radius = cords.left = ${ball.offsetWidth / 2}`);
-
+const posTop = cords.top + window.scrollY;
+const posLeft = cords.left + window.scrollX;
 
 field.addEventListener('click', placeBall);
-// body.addEventListener('mousemove', info);
-
-
-function info(event) {
-  if (event.clientY < cords.top + borderWidth) {
-    // body.style.background = 'green';
-  } else {
-    // body.style.background = 'red';
-  }
-
-  if (event.clientY > cords.bottom - borderWidth) {
-    // body.style.background = 'green';
-  } else {
-    // body.style.background = 'red';
-  }
-
-  if (event.clientX < cords.left + borderWidth) {
-    // body.style.background = 'green';
-  } else {
-    // body.style.background = 'red';
-  }
-
-  if (event.clientX > cords.right - borderWidth - window.scrollX) {
-    body.style.background = 'green';
-  } else {
-    body.style.background = 'red';
-  }
-}
-
 function placeBall(event) {
-
-  console.log(event);
-
-  console.log(posTop);
-  console.log(posTop + window.scrollY);
+  let topCoordinate = event.clientY - posTop + window.scrollY;
+  let leftCoordinate = event.clientX - posLeft + event.pageX - event.clientX;
 
   if (event.clientY < posTop - window.scrollY + borderWidth + ballRadius) {
-    ball.style.cssText = `top:${borderWidth + ballRadius}px;
-   margin-top: -${(ball.offsetHeight / 2) + borderWidth}px;
-   margin-left: -${(ball.offsetWidth / 2) + borderWidth}px;
-   left:${event.clientX - posLeft + (event.pageX - event.clientX)}px`;
-    console.log(`posTop + borderWidth + ballRadius = ${posTop + borderWidth + ballRadius}`);
-    console.log(`event.clientY = ${event.clientY}`);
-    return;
+    topCoordinate = borderWidth + ballRadius;
   }
 
   if (event.clientY > (posTop + cords.height - window.scrollY) - (borderWidth + ballRadius)) {
-    ball.style.cssText = `top:${cords.height - (borderWidth + ballRadius)}px;
-   margin-top: -${(ball.offsetHeight / 2) + borderWidth}px;
-   margin-left: -${(ball.offsetWidth / 2) + borderWidth}px;
-   left:${event.clientX - posLeft + (event.pageX - event.clientX)}px`;
-    return;
+    topCoordinate = cords.height - (borderWidth + ballRadius);
   }
 
-  if (event.clientX < posLeft + (borderWidth + ballRadius)) {
-    ball.style.cssText = `top:${(event.clientY - (posTop - window.scrollY))}px;
-   margin-top: -${(ball.offsetHeight / 2) + borderWidth}px;
-   margin-left: -${(ball.offsetWidth / 2) + borderWidth}px;
-   left:${borderWidth + ballRadius}px`;
-    return;
+  if (event.clientX < posLeft + (borderWidth + ballRadius) - scrollX) {
+    leftCoordinate = borderWidth + ballRadius;
   }
 
-  if (event.clientX > cords.right - (borderWidth + ballRadius + scrollX)) {
-    ball.style.cssText = `top:${(event.clientY - (posTop - window.scrollY))}px;
-     margin-top: -${(ball.offsetHeight / 2) + borderWidth}px;
-     margin-left: -${(ball.offsetWidth / 2) + borderWidth}px;
-     left: ${cords.width - (borderWidth + ballRadius)}px`;
-    return;
+  if (event.clientX > posLeft + cords.width - (borderWidth + ballRadius) - scrollX) {
+    leftCoordinate = cords.width - (borderWidth + ballRadius);
   }
 
-  ball.style.cssText = `top:${(event.clientY - (posTop - window.scrollY))}px;
-   margin-top: -${(ball.offsetHeight / 2) + borderWidth}px;
-   margin-left: -${(ball.offsetWidth / 2) + borderWidth}px;
-   left:${event.clientX - posLeft + (event.pageX - event.clientX)}px`;
+  ball.style.cssText = `top:${topCoordinate}px;
+   margin-top: -${ballRadius + borderWidth}px;
+   margin-left: -${ballRadius + borderWidth}px;
+   left:${leftCoordinate}px`;
 }
-

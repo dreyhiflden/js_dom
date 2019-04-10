8810ae29b7592759be1f797361104d24cfae0dd4
dreyhiflden <dreyhiflden@gmail.com>
2019-04-10 17:04:35 +0300

    Add helper functions

--- ball.js
+++ ball.js
@@ -5,25 +5,30 @@ const borderWidth = (field.offsetWidth - field.clientWidth) / 2;
 const cords = field.getBoundingClientRect();
 const posTop = cords.top + window.scrollY;
 const posLeft = cords.left + window.scrollX;
+const getMinY = () => posTop - window.scrollY + borderWidth + ballRadius;
+const getMaxY = () => (posTop + cords.height - window.scrollY) - (borderWidth + ballRadius);
+const getMinX = () => posLeft + (borderWidth + ballRadius) - scrollX;
+const getMaxX = () => posLeft + cords.width - (borderWidth + ballRadius) - scrollX;
 
 field.addEventListener('click', placeBall);
+
 function placeBall(event) {
   let topCoordinate = event.clientY - posTop + window.scrollY;
   let leftCoordinate = event.clientX - posLeft + event.pageX - event.clientX;
 
-  if (event.clientY < posTop - window.scrollY + borderWidth + ballRadius) {
+  if (event.clientY < getMinY()) {
     topCoordinate = borderWidth + ballRadius;
   }
 
-  if (event.clientY > (posTop + cords.height - window.scrollY) - (borderWidth + ballRadius)) {
+  if (event.clientY > getMaxY()) {
     topCoordinate = cords.height - (borderWidth + ballRadius);
   }
 
-  if (event.clientX < posLeft + (borderWidth + ballRadius) - scrollX) {
+  if (event.clientX < getMinX()) {
     leftCoordinate = borderWidth + ballRadius;
   }
 
-  if (event.clientX > posLeft + cords.width - (borderWidth + ballRadius) - scrollX) {
+  if (event.clientX > getMaxX()) {
     leftCoordinate = cords.width - (borderWidth + ballRadius);
   }
 
@@ -31,4 +36,4 @@ function placeBall(event) {
    margin-top: -${ballRadius + borderWidth}px;
    margin-left: -${ballRadius + borderWidth}px;
    left: ${leftCoordinate}px`;
-};
+}
